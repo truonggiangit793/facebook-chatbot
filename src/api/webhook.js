a4e077f1c5ba110f5cce13a4ec832e3d76d9a277
truonggiangit793 <truonggiangit793@gmail.com>
2023-04-09 15:52:25 +0700

    Update webhook

--- src/api/webhook.js
+++ src/api/webhook.js
@@ -1,5 +1,8 @@
 import express from "express";
+import axios from "axios";
+
 const Router = express.Router();
+const PAGE_ACCESS_TOKEN = process.env.PAGE_ACCESS_TOKEN || null;
 
 Router.get("/", (req, res, next) => {
     let mode = req.query["hub.mode"];
@@ -18,8 +21,60 @@ Router.get("/", (req, res, next) => {
 Router.post("/", (req, res, next) => {
     let body = req.body;
     console.log(`\u{1F7EA} Received webhook:`);
-    console.dir(body, { depth: null });
+    console.dir(body);
+    // body = {
+    //     object: "page",
+    //     entry: [
+    //         {
+    //             id: "100711156323546",
+    //             time: 1681029570395,
+    //             messaging: [
+    //                 {
+    //                     sender: { id: "5982973831786425" },
+    //                     recipient: { id: "100711156323546" },
+    //                     timestamp: 1681029570128,
+    //                     message: {
+    //                         mid: "m_KZKgBVbmFolLKaXRQ_JrK_bzXB6LW-VzV4JiFJTeiYgDF3RCUZK7ds_czSCkW4oHxBY4TBHVMN-Fzq5r7QOQiA",
+    //                         text: "Hi",
+    //                         nlp: {
+    //                             intents: [],
+    //                             entities: {},
+    //                             traits: {
+    //                                 wit$greetings: [
+    //                                     {
+    //                                         id: "5900cc2d-41b7-45b2-b21f-b950d3ae3c5c",
+    //                                         value: "true",
+    //                                         confidence: 0.9999,
+    //                                     },
+    //                                 ],
+    //                                 wit$sentiment: [
+    //                                     {
+    //                                         id: "5ac2b50a-44e4-466e-9d49-bad6bd40092c",
+    //                                         value: "positive",
+    //                                         confidence: 0.7336,
+    //                                     },
+    //                                 ],
+    //                             },
+    //                             detected_locales: [{ locale: "vi_VN", confidence: 0.8365 }],
+    //                         },
+    //                     },
+    //                 },
+    //             ],
+    //         },
+    //     ],
+    // };
     if (body.object === "page") {
+        if (body.entry[0].messaging && body.entry[0].messaging[0].message.text) {
+            axios.post("https://graph.facebook.com/v16.0/100711156323546/messages?access_token=" + process.env.PAGE_ACCESS_TOKEN, {
+                recipient: {
+                    id: body.entry[0].messaging[0].sender.id,
+                },
+                messaging_type: "RESPONSE",
+                message: {
+                    text: "Hello, world!",
+                },
+            });
+        }
         return res.status(200).send("EVENT_RECEIVED");
     } else {
         return res.sendStatus(404);
