1340a0874c14d873ff1c73e3a0c5f3b6aaa63b6a
truonggiangit793 <truonggiangit793@gmail.com>
2023-04-10 00:48:53 +0700

    Update code

--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,16 @@
 import express from "express";
 const Router = express.Router();
 
+import platformOpenAI from "../services/platform.openai";
+
 Router.get("/", (req, res, next) => {
     return res.end("Hello world!");
 });
 
-Router.get("/test/:message", async (req, res, next) => {});
+Router.get("/test/:message", async (req, res, next) => {
+    const message = req.params.message;
+    const response = await platformOpenAI.createCompletion(message);
+    res.json(response);
+});
 
 export default Router;
