09f2b4e6c7be4eb3eb84eff1b5fa11cdb270d194
truonggiangit793 <truonggiangit793@gmail.com>
2023-04-11 10:44:13 +0700

    Update code

--- src/services/platform.openai.js
+++ src/services/platform.openai.js
@@ -2,12 +2,12 @@ import configs from "../../env.config";
 import { Configuration, OpenAIApi } from "openai";
 
 const createCompletion = async function (message) {
-    const temperature = Math.random() * 2;
+    // const temperature = Math.random() * 2;
     const configuration = new Configuration({ apiKey: configs.CHAT_GPT_API_KEY });
     const openai = new OpenAIApi(configuration);
     try {
         const completion = await openai.createChatCompletion({
-            temperature,
+            // temperature,
             model: "gpt-3.5-turbo",
             messages: [{ role: "user", content: message }],
         });
