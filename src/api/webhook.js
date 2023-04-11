b08c7bdef3cfc0daf6752d909ec21caf393b02bc
truonggiangit793 <truonggiangit793@gmail.com>
2023-04-11 15:57:50 +0700

    Update code

--- src/api/webhook.js
+++ src/api/webhook.js
@@ -22,21 +22,23 @@ Router.post("/", async (req, res, next) => {
     if (body.object === "page") {
         await body.entry.forEach(async function (entry) {
             console.dir(entry, { depth: null });
-            // Get the body of webhook event
-            let webhook_event = entry.standby[0];
-            // Get the sender psid
-            let sender_psid = webhook_event.sender.id;
-            // Check if the event is a message or postback then pass it through the handler function
-            if (webhook_event.message) {
-                // Send the sender actions
-                messengerPlatform.handleSenderAction(sender_psid, "mark_seen");
-                // Send the sender actions
-                messengerPlatform.handleSenderAction(sender_psid, "typing_on");
-                // Handle message
-                await messengerPlatform.handleMessage(sender_psid, webhook_event.message);
-            } else if (webhook_event.postback) {
-                // Handle postback
-                await messengerPlatform.handlePostback(sender_psid, webhook_event.postback);
+            if (entry.messaging) {
+                // Get the body of webhook event
+                let webhook_event = entry.messaging[0];
+                // Get the sender psid
+                let sender_psid = webhook_event.sender.id;
+                // Check if the event is a message or postback then pass it through the handler function
+                if (webhook_event.message) {
+                    // Send the sender actions
+                    messengerPlatform.handleSenderAction(sender_psid, "mark_seen");
+                    // Send the sender actions
+                    messengerPlatform.handleSenderAction(sender_psid, "typing_on");
+                    // Handle message
+                    await messengerPlatform.handleMessage(sender_psid, webhook_event.message);
+                } else if (webhook_event.postback) {
+                    // Handle postback
+                    await messengerPlatform.handlePostback(sender_psid, webhook_event.postback);
+                }
             }
         });
         return res.status(200).send("EVENT_RECEIVED");
